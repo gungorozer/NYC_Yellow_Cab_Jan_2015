5a9643d199eaa76a307f253e1ddd16a4582c14a9
Gungor Ozer <gungor.ozer@gmail.com>
2016-04-18 18:48:19 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html><head><title><?php echo "cool title";?></title></head>
+<body
+<?php 
+$image_url='http://i282.photobucket.com/albums/kk277/dersimden/cab_rides_Jan_2016_summary.jpg';
+?>
+
+<img src="<?php echo $image_url;?>">
+
+<br><h1>Got me?</h1>
